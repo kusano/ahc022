484f40f27bb0fba21e736d810188a9211b6f6a40
kusano <kusano@users.noreply.github.com>
2023-08-17 01:37:51 +0900

    なるべく当てにいく
    6106696
    157,959,022

--- A.cpp
+++ A.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <set>
 using namespace std;
+
+int xor64() {
+    static uint64_t x = 88172645463345263ULL;
+    x ^= x<<13;
+    x ^= x>> 7;
+    x ^= x<<17;
+    return int(x&0x7fffffff);
+}
 
 int main()
 {
@@ -12,36 +21,55 @@ int main()
     for (int i=0; i<N; i++)
         cin>>Y[i]>>X[i];
 
-    vector<int> I;
-    for (int i=0; i<N/2; i++)
+    vector<vector<int>> PP(L, vector<int>(L, -1));
+    vector<int> DX, DY;
+    vector<vector<int>> EI(N);
+    while (true)
     {
-        I.push_back(i);
-        I.push_back(N-i-1);
+        int dx, dy;
+        while (true)
+        {
+            dx = xor64()%21-10;
+            dy = xor64()%21-10;
+            if (abs(dx)+abs(dy)<=10) {
+                bool ok = true;
+                for (int i=0; i<(int)DX.size(); i++)
+                    if (dx==DX[i] && dy==DY[i])
+                        ok = false;
+                if (ok)
+                    break;
+            }
+        }
+        DX.push_back(dx);
+        DY.push_back(dy);
+
+        for (int i=0; i<N; i++)
+        {
+            int tx = (X[i]+dx+L)%L;
+            int ty = (Y[i]+dy+L)%L;
+            int b = -1;
+            if (PP[ty][tx]==-1)
+            {
+                b = xor64()%2;
+                PP[ty][tx] = b;
+            }
+            else
+                b = PP[ty][tx];
+            EI[i].push_back(b);
+        }
+        if (set<vector<int>>(EI.begin(), EI.end()).size()==N)
+            break;
     }
-    if (N%2!=0)
-        I.push_back(N/2);
 
     vector<vector<int>> P(L, vector<int>(L, 500));
-    vector<vector<bool>> PF(L, vector<bool>(L));
-    for (int i=0; i<N; i++)
-    {
-        P[Y[I[i]]][X[I[i]]] = 1000*i/(N-1);
-        PF[Y[I[i]]][X[I[i]]] = true;
-    }
-
-    for (int i=0; i<100; i++)
-    {
-        vector<vector<int>> T = P;
-        for (int y=0; y<L; y++)
-            for (int x=0; x<L; x++)
-                if (PF[y][x])
-                    P[y][x] = T[y][x];
-                else
-                {
-                    int s = (T[(y+L-1)%L][x] + T[(y+1)%L][x] + T[y][(x+L-1)%L] + T[y][(x+1)%L]+2)/4;
-                    P[y][x] = s;
-                }
-    }
+    for (int y=0; y<L; y++)
+        for (int x=0; x<L; x++)
+        {
+            if (PP[y][x]==-1)
+                P[y][x] = 500;
+            else
+                P[y][x] = PP[y][x]*1000;
+        }
 
     for (int y=0; y<L; y++)
     {
@@ -53,27 +81,25 @@ int main()
     vector<int> E(N);
     for (int i=0; i<N; i++)
     {
-        vector<int> T;
-        for (int j=0; j<99; j++)
+        vector<int> B;
+        for (int j=0; j<(int)DX.size(); j++)
         {
-            cout<<i<<" "<<0<<" "<<0<<endl;
-            int t;
-            cin>>t;
-            T.push_back(t);
-        }
-        sort(T.begin(), T.end());
-        int t = T[49];
-
-        int m = 9999;
-        for (int j=0; j<N; j++)
-        {
-            int d = abs(P[Y[j]][X[j]]-t);
-            if (d<m)
+            vector<int> T;
+            for (int k=0; k<5; k++)
             {
-                m = d;
-                E[i] = j;
+                cout<<i<<" "<<DY[j]<<" "<<DX[j]<<endl;
+                int t;
+                cin>>t;
+                T.push_back(t);
             }
+            sort(T.begin(), T.end());
+            B.push_back(T[2]>500?1:0);
         }
+        int e = 0;
+        for (int j=0; j<N; j++)
+            if (B==EI[j])
+                e = j;
+        E[i] = e;
     }
 
     cout<<-1<<" "<<-1<<" "<<-1<<endl;
